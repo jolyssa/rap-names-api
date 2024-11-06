b22ebabef334d758c3e8124420766678117876ac
jolyssa <jolyssa.armstrong@gmail.com>
2024-11-05 20:31:49 -0500

    first api

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+    'age': 29,
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England',
+    },
+
+    'chance the rapper': {
+    'age': 29,
+    'birthName': 'Chancellor Bennett',
+    'birthLocation': 'Chicago, Illinois',
+    },
+
+    'unknown': {
+    'age': 0,
+    'birthName': 'unknown',
+    'birthLocation': 'unknown',
+    },
+}
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// for API's
+app.get('/api/:name', (req,res) => {
+    const rapperName = req.params.name.toLowerCase()
+
+    // console.log(rappers[rapperName].birthName)
+    
+    if (rappers[rapperName]){
+        res.json(rappers[rapperName])
+    } else {
+        res.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on ${PORT}.`)
+})
